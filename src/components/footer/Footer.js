287773b5f68b0bdeecab87ae90ad7ef50d407a6e
MohaDicko <mohadicko95@gmail.com>
2026-01-27 23:12:18 +0000

    fix: resolve build warnings and fetch.js typo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,12 @@
-import React, {useContext, useState} from "react";
+import React, { useState } from "react";
 import "./Footer.scss";
-import {Fade} from "react-reveal";
+import { Fade } from "react-reveal";
 import emoji from "react-easy-emoji";
-import StyleContext from "../../contexts/StyleContext";
+
 import axios from "axios";
 
 export default function Footer() {
-  // const {isDark} = useContext(StyleContext);
+
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
@@ -83,9 +83,8 @@ export default function Footer() {
           </form>
           {notification && (
             <div
-              className={`notification-pill ${
-                notification.includes("success") ? "success" : "error"
-              }`}
+              className={`notification-pill ${notification.includes("success") ? "success" : "error"
+                }`}
             >
               {notification === "Message sent successfully!"
                 ? "✅ Message envoyé avec succès !"
